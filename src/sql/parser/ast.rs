dc808146f12443f83f9c64ebb6fb9d5c8b0bfad3
Rory Li <rory.li@opus.pro>
2025-11-22 23:48:53 +0800

    init

---
+++ src/sql/parser/ast.rs
@@ -0,0 +1,52 @@
+// Abstract Syntax Tree
+#[derive(Debug, PartialEq)]
+pub enum Statement {
+    CreateTable {
+        name: String,
+        columns: Vec<Column>,
+    },
+    Insert {
+        table_name: String,
+        columns: Option<Vec<String>>,
+        values: Vec<Vec<Expression>>,
+    },
+    Select {
+        table_name: String,
+    },
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum DataType {
+    Boolean,
+    Float,
+    Integer,
+    String,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Column {
+    pub name: String,
+    pub datatype: DataType,
+    pub nullable: Option<bool>,
+    pub default: Option<Expression>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Expression {
+    Consts(Consts),
+}
+
+impl From<Consts> for Expression {
+    fn from(value: Consts) -> Self {
+        Self::Consts(value)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Consts {
+    Null,
+    Boolean(bool),
+    Integer(i64),
+    Float(f64),
+    String(String),
+}
